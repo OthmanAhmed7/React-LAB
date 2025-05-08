cddb2e7daade150316cbb4747b2609139bd86c59
Othman Ahmed <othmanahmed258@gmail.com>
2025-05-08 22:57:02 +0300

    add language context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import ProductsList from "./pages/ProductsList";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
+import LanguageContext from "./context/Language";
 
 function App() {
+  const [language, setLanguage] = useState(false);
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ProductsList />} />
-          <Route path="/product-details/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <LanguageContext.Provider value={{ language, setLanguage }}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductsList />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </LanguageContext.Provider>
     </>
   );
 }
